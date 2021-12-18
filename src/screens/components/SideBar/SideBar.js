feef4a31846eec4ec6567b271bf67a714bb848a9
Peter M <piotr.maximer@gmail.com>
2021-12-18 15:19:11 +0100

    Changed SideBar and added icons support

--- src/screens/components/SideBar/SideBar.js
+++ src/screens/components/SideBar/SideBar.js
@@ -1,20 +1,42 @@
 import "./SideBar.css";
 import React, { useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 export default function SideBar(props) {
 
     const [sideBarCollapsed, setSideBarCollapsed] = useState(true);
 
     return (
-        <div className={"side-bar" + (sideBarCollapsed ? "" : " side-bar-expanded" )}>
+        <div className={"sideBar" + (sideBarCollapsed ? "" : " sideBarExpanded")}>
             {sideBarCollapsed &&
-                <div className="sideBarExpandButton" onClick={() => { setSideBarCollapsed(false) }}>
-                    |||
+                <div className="sideBarButton" title="Pokaż zapisane plany" onClick={() => { setSideBarCollapsed(false) }}>
+                    <FontAwesomeIcon icon={faBars} />
                 </div>
             }
             {!sideBarCollapsed &&
-                <div className="sideBarCollapseButton" onClick={() => { setSideBarCollapsed(true) }}>
-                    V
+                <div className="sideBarTopRow">
+                    <div className="sideBarButton" onClick={() => { setSideBarCollapsed(true) }}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </div>
+                    <div className="sideBarTitleText">
+                        Zapisane plany
+                    </div>
+                </div>
+
+            }
+            {!sideBarCollapsed &&
+                <div className="sideBarContent">
+                    <div className="planListEntry">
+                        Plan zajęć nr.2 - z konsultacjami nauczycieli
+                    </div>
+                    <div className="planListEntry">
+                        Plan zajęć (semestr 3) bez grup Ps
+                    </div>
+                    <div className="planListEntry">
+                        Main plan zajęć
+                    </div>
                 </div>
             }
         </div>
