a6baf86de319bd93b35298e181d86449ae157ea8
WooooCash <lukaszbialczak3@gmail.com>
2021-12-18 15:07:39 +0100

    styling navbar

--- src/screens/components/NavBar/NavBar.js
+++ src/screens/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
 
     return (
         <div className="navbar">
-            <div className="navbar-button navbar-button-selected">Home</div>
+            <div className="navbar-button navbar-button-selected">Nowy Plan</div>
             <div className="navbar-button">Nauczyciele</div>
         </div>
     );
