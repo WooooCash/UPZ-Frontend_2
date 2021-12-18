e080396833807a1a4276abefc1b312ca31a64317
WooooCash <lukaszbialczak3@gmail.com>
2021-12-18 16:37:20 +0100

    Added animation to navbar button hover

--- src/screens/components/NavBar/NavBar.js
+++ src/screens/components/NavBar/NavBar.js
@@ -1,12 +1,28 @@
 import './NavBar.css'
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarPlus, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
 
     return (
         <div className="navbar">
-            <div className="navbar-button navbar-button-selected">Nowy Plan</div>
-            <div className="navbar-button">Nauczyciele</div>
+            <div className="navbar-button navbar-button-selected">
+                <div className="navbar-button-icon">
+                    <FontAwesomeIcon icon={faCalendarPlus} />
+                </div>
+                <div className="navbar-button-text"> 
+                    Nowy Plan
+                </div>
+            </div>
+            <div className="navbar-button">
+                <div className="navbar-button-icon">
+                    <FontAwesomeIcon icon={faUser} />
+                </div>
+                <div className="navbar-button-text"> 
+                    Nauczyciele
+                </div>
+            </div>
         </div>
     );
 }
