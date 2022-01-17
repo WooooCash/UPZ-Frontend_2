776609e9c12a8ba499856601421db249c9d5337a
Peter M <piotr.maximer@gmail.com>
2022-01-17 14:07:49 +0100

    Added routing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import MainScreen from "./screens/main/MainScreen.js";
 import ConsultationsScreen from './screens/consultations/ConsultationsScreen';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConsultationsScreen />
+    <Router>
+      <Route exact path="/consultations" component={ConsultationsScreen} />
+      <Route exact path="/" component={MainScreen}/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/screens/components/NavBar/NavBar.js
+++ src/screens/components/NavBar/NavBar.js
@@ -2,27 +2,32 @@ import './NavBar.css'
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarPlus, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
 
     return (
         <div className="navbar">
-            <div className="navbar-button navbar-button-selected">
-                <div className="navbar-button-icon">
-                    <FontAwesomeIcon icon={faCalendarPlus} />
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <div className="navbar-button navbar-button-selected">
+                    <div className="navbar-button-icon">
+                        <FontAwesomeIcon icon={faCalendarPlus} />
+                    </div>
+                    <div className="navbar-button-text"> 
+                        Nowy Plan
+                    </div>
                 </div>
-                <div className="navbar-button-text"> 
-                    Nowy Plan
+            </Link>
+            <Link to="/consultations" style={{ textDecoration: 'none' }}>
+                <div className="navbar-button">
+                    <div className="navbar-button-icon">
+                        <FontAwesomeIcon icon={faUser} />
+                    </div>
+                    <div className="navbar-button-text"> 
+                        Nauczyciele
+                    </div>
                 </div>
-            </div>
-            <div className="navbar-button">
-                <div className="navbar-button-icon">
-                    <FontAwesomeIcon icon={faUser} />
-                </div>
-                <div className="navbar-button-text"> 
-                    Nauczyciele
-                </div>
-            </div>
+            </Link>
         </div>
     );
 }
