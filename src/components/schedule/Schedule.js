605d6e53d22a81b662ad9ff51e7acce1d3f7e6ff
WooooCash <lukaszbialczak3@gmail.com>
2022-01-30 23:28:15 +0100

    added schedule mockup

---
+++ src/components/schedule/Schedule.js
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import "./Schedule.css"
+
+export default function Schedule() {
+    
+    return(
+        <div className="container">
+
+            <div className="schedule__container">
+                <div className="days__container">
+                    <span className="corner"></span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+                    <span className="time">8.30 <br /> - <br /> 9.15</span>
+
+                </div>
+                <div className="part__day">
+                    <div className="day">Pon</div>
+
+                    <div className="task_schedule">Komponenty</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task_schedule2">KCK</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+
+                </div>
+                <div className="part__day">
+                    <div className="day">Wt</div>
+
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task_schedule2">SM</div>
+                    <div className="task_schedule2">ANG</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+
+                </div>
+                <div className="part__day">
+                    <div className="day">Śr</div>
+
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    
+                </div>
+                <div className="part__day">
+                    <div className="day">Czw</div>
+
+                    <div className="task_schedule2">SBD</div>
+                    <div className="task_schedule2">ZTP</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task_schedule">PZ</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                </div>
+                <div className="part__day">
+                    <div className="day">Pt</div>
+
+                    <div className="task_schedule2">VR</div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                </div>
+                <div className="part__day">
+                    <div className="day">Sob</div>
+
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                </div>
+                <div className="part__day">
+                    <div className="day">Ndz</div>
+
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                    <div className="task"></div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainScreen from "./screens/main/MainScreen.js";
 import ConsultationsScreen from './screens/consultations/ConsultationsScreen';
+import Schedule from './components/schedule/Schedule';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Route exact path="/consultations" component={ConsultationsScreen} />
+      <Route exact path="/schedule" component={Schedule} />
       <Route exact path="/" component={MainScreen}/>
     </Router>
   </React.StrictMode>,
