ed18c0ddffc1da930b0d74804682f55f92e68564
WooooCash <lukaszbialczak3@gmail.com>
2022-04-05 18:53:44 +0200

    display consultation plan from server

--- src/screens/consultations/ConsultationsView/Consultations.js
+++ src/screens/consultations/ConsultationsView/Consultations.js
@@ -1,24 +1,25 @@
 import React, {useState, useEffect} from "react";
 import { getConsultations, getTeachers } from "../../../util/api.js"
+import PlanWeekView from '../../plan/PlanWeekView/PlanWeekView'
 import "./Consultations.css"
 
 
 
 const hours = [
-    ["8.30", "9.15"],
-    ["9.15", "10.00"],
-    ["10.15", " 11.00"],
-    ["11.00", " 11.45"],
-    ["12.00", " 12.45"],
-    ["12.45", " 13.30"],
-    ["14.00", " 14.45"],
-    ["14.45", " 15.30"],
-    ["16.00", " 16.45"],
-    ["16.45", " 17.30"],
-    ["17.40", " 18.25"],
-    ["18.25", " 19.10"],
-    ["19.20", " 20.05"],
-    ["20.05", " 20.50"],
+    ["8:30", "9:15"],
+    ["9:15", "10:00"],
+    ["10:15", "11:00"],
+    ["11:00", "11:45"],
+    ["12:00", "12:45"],
+    ["12:45", "13:30"],
+    ["14:00", "14:45"],
+    ["14:45", "15:30"],
+    ["16:00", "16:45"],
+    ["16:45", "17:30"],
+    ["17:40", "18:25"],
+    ["18:25", "19:10"],
+    ["19:20", "20:05"],
+    ["20:05", "20:50"],
 ]
 
 export default function Conultations(props) {
@@ -27,10 +28,12 @@ export default function Conultations(props) {
     const [teachers, setTeachers] = useState({})
     const [currentProf, setCurrentProf] = useState(0)
     const [profName, setProfName] = useState("")
+    const [events, setEvents] = useState([])
 
 
     useEffect(() => {
-        let jsonArrConsultations = getConsultations().then(result => {
+        console.log("api call")
+        getConsultations().then(result => {
             let tempData = {}
             for(let cons of result.fetchTutorships) {
                 tempData[cons.teacherId] = {}
@@ -38,40 +41,47 @@ export default function Conultations(props) {
             for(let cons of result.fetchTutorships) {
                 tempData[cons.teacherId][cons.id] = cons
             }
-            console.log("consultations", tempData)
             setConsultations(tempData);
         });
-        let jsonArrTeachers = getTeachers().then(result => {
+        getTeachers().then(result => {
             let tempData = {}
             for(let teacher of result.fetchTeachers) {
                 let fullName = teacher.lastName
                 if (teacher.name != null)
                     fullName = teacher.name + " " + teacher.lastName;
-                if (teacher.lastName.slice(0, 1) != "(")
-                    tempData[fullName] = teacher;
+
+                tempData[fullName] = teacher;
             }
-            console.log("teachers", tempData)
             setTeachers(tempData);
         });
     }, [])
 
     const switchProf = (e) => {
-        setCurrentProf(e.target.value)
-        setProfName(e.target.options[e.target.selectedIndex].text)
-        console.log("currProf", e.target.value)
-        console.log("profName", e.target.options[e.target.selectedIndex].text)
+        let profId = e.target.value;
+        let profName = e.target.options[e.target.selectedIndex].text;
+        setCurrentProf(profId)
+        setProfName(profName)
+        prepareData(profId, profName);
     }
 
-    const showCurrentProfCons = () => {
-        console.log(consultations[currentProf])
-        Object.entries(consultations[currentProf]).map((consId) => {
-            console.log(consId[0], consId[1])
+    const prepareData = (id, name) => {
+        var evs = Object.entries(consultations[id]).map((consId) => {
+            var event = {
+                dayNumber: consId[1].day.toString(),
+                from: hours[consId[1].hour-1][0],
+                to: hours[consId[1].hour-1][1],
+                color: '#64c954',
+                person: name,
+                other: consId[1].description
+            }
+
+            return event;
         })
+        setEvents(evs)
     }
 
     return(
         <div>
-            <button onClick={showCurrentProfCons}>show</button>
             <div className="select-form">
                 <label>Wybierz profesora</label>
                 <br />
@@ -85,21 +95,7 @@ export default function Conultations(props) {
 
             <div className="schedule-view">
                 {!consultations[currentProf] && (<p>Nothing to see here B)</p>)}
-                { consultations[currentProf] && Object.entries(consultations[currentProf]).map((consId) => (
-                    <div>
-                        <h2>Consultation ID: {consId[0]}</h2>
-                        <div style={{marginLeft:"40px"}}>
-                            <p>Hour: {consId[1].hour}</p>
-                            <p>From: {hours[consId[1].hour-1][0]}</p>
-                            <p>To: {hours[consId[1].hour-1][1]}</p>
-                            <p>dayNumber: {consId[1].day}</p>
-                            <p>color: green</p>
-                            <p>Teacher ID: {consId[1].teacherId}</p>
-                            <p>person: {profName}</p>
-                            <p>other: {consId[1].description}</p>
-                        </div>
-                    </div>
-                ))}
+                {consultations[currentProf] && (<PlanWeekView events={events}/>)}
             </div>
         </div>
     )
